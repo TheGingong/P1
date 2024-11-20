0cbef47af38c87685f388e996cb65b0af915b9b1
Gakki <mathias@kejser.eu>
2024-11-20 15:34:49 +0100

    convert added

---
+++ src/Convert.c
@@ -0,0 +1,5 @@
+//
+// Created by Mathias Kejser on 20/11/2024.
+//
+
+#include "Convert.h"
---
+++ src/Convert.h
@@ -0,0 +1,8 @@
+//
+// Created by Mathias Kejser on 20/11/2024.
+//
+
+#ifndef CONVERT_H
+#define CONVERT_H
+
+#endif //CONVERT_H
