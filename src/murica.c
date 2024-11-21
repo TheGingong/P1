c57dcf7701709b28234726080dfb4c1858eb9def
SorenAAU <sesc24@student.aau.dk>
2024-11-21 12:58:17 +0100

    Added comments

--- src/murica.c
+++ src/murica.c
@@ -3,21 +3,32 @@
 
 #define NUMBER_CANDIDATES 5
 #define STATES 50
+#define NUMBER_OF_VOTERS 500
 
 // Array[] = cali(A0.9, B0.7, C0.6, D0.5, E0.4)
 typedef struct {
     int stat;
     char pref;
-    int electors;
-} states;
+} person;
 
-void murica(person s, person c) {
-    for (int i = 0; i < NUMBER_CANDIDATES; i++) {
-      printf("%d", c);
-    }
-    printf("%d", s);
 
-    for (int i = 0; i < STATES; i++) {
-        convert_america();
+void america() {
+    person x;
+
+
+    for(int i = 0; i < NUMBER_OF_VOTERS; i++) {
+
+
     }
+
 }
+
+//Lav et array for hvert stat (evt i et struct), med n antal kandidater.
+
+//I loopet checkes hver stemme for stat og kandidat, og tæller kandidaten op i staten.
+
+//Derefter laves en funktion, som kan udregne vinderen i den enkelte stat.
+
+//Udregn antal valgmænd???
+
+//Det store regnestykke
