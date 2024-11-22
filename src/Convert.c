e62a01bd136cd149234bbea31e95d218aec0e842
David Bach <60426203+DrMachoo@users.noreply.github.com>
2024-11-22 09:20:23 +0100

    Fixed typecast issue and update convert_america

--- src/Convert.c
+++ src/Convert.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include<math.h>
-#define MAX_LINE_LENGTH 256
+#define MAX_LINE_LENGTH 40
 #define NUMBER_OF_CANDIDATES 5
 
 
@@ -36,28 +36,29 @@ char* convert(int line_number) {
     }
 }
 
-struct person convert_america(int i) {
+struct person convert_america(int line_person) {
     struct person ny_person;
     FILE *file = fopen("text-files/test-tekstil.txt","r");
     if (file == NULL) {
         perror("Could not open file");
     }
 
-    char text_string[MAX_LINE_LENGTH];
+    char temp_text_str[MAX_LINE_LENGTH];
     int current_line = 0;
 
     // fgets læser max "sizeof(text_string) characters fra file stream -
     // - og gemmer dem i string-arrayet "text_string"
     // fgets stopper med at parse text hvis den rammer en newline
-    while (fgets(text_string, sizeof(text_string), file) != NULL) {
-        if (current_line == i) {
-            int temp;
+    while (fgets(temp_text_str, sizeof(temp_text_str), file) != NULL) {
+        if (current_line == line_person) {
+            char temp;
             // printf("Line %d: %s", i, text_string);
             // Sscanf det array hvori fgets har parset text dokumentet til
             // herefter bliver dette skrevet ind i de variabler der er specificeret
-            if (sscanf(text_string, "%d( %c", &ny_person.stat, &temp) == 2) {
-                ny_person.pref = Lav_tal_om_for_soren(temp);
-                printf("stat: %d\nPref: %d\n", ny_person.stat, ny_person.pref);
+            if (sscanf(temp_text_str, "%d( %c", &ny_person.stat, &temp) == 2) {
+                // typecasting the char to an in
+                ny_person.pref = temp - 'A';
+                //printf("stat: %d\nPref: %d\n", ny_person.stat, ny_person.pref);
             } else {
                 printf("Error: Could not parse the line.\n");
             }
@@ -66,13 +67,17 @@ struct person convert_america(int i) {
         }
         current_line++;
     }
-    if (current_line < i) {
-        printf("The file has fewer than %d lines.\n", i);
+    if (current_line < line_person) {
+        printf("The file has fewer than %d lines.\n", line_person);
     }
+        // If fgets has read to the last line it returns "ny_person.stat" with -1 to signal the end of the txt
+        fclose(file);
+        ny_person.stat = -1;
+        return ny_person;
 
-    fclose(file);
 }
 
+// kode thats is ligegyldigt
 int Lav_tal_om_for_soren(int tal) {
     for (int i = 0, j = 65; ; i++, j++) {
         if ((int)tal == j) {
